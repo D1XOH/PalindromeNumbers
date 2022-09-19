351fb58a6d10d6dd1238262ce31309deab0dc4e5
D1XOH <70796382+D1XOH@users.noreply.github.com>
2022-09-19 18:37:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Palindrome1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 100000; i < 999999; i++)
+            {
+                int num = i;
+                int N = 0;
+                while(num != 0)
+                {
+                    int digit = num % 10;
+                    N = N * 10 + digit;
+                    num = num / 10;
+                }
+                if(i == N)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+
+        }
+    }
+}
